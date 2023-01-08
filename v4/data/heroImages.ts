441213744c74483f917c222536343bb1efb79317
Arash <47273556+arashnrim@users.noreply.github.com>
2023-01-08 18:20:22 +0800

    feat: add hero section

---
+++ v4/data/heroImages.ts
@@ -0,0 +1,24 @@
+const images = [
+  {
+    src: "/images/epoch.jpg",
+    alt: "A person standing in between two other people looking to the side.",
+    text: "That's me coordinating something at Epoch, Hack Club's 42-hour hackathon in 2022.",
+  },
+  {
+    src: "/images/sis.jpg",
+    alt: "Two people standing separated by a computer presenting to another person standing across them.",
+    text: "That's me presenting at the Swift Innovators' Summit 2019.",
+  },
+  {
+    src: "/images/github.jpg",
+    alt: "A group of six people standing in front of a logo of Microsoft with balloons spelling out GitHub below it.",
+    text: "That's me and a group of friends at the GitHub Universe Singapore watch party.",
+  },
+  {
+    src: "/images/homecoming.jpg",
+    alt: "A group of five people sitting and squatting on a field.",
+    text: "That's me with a group of friends at my school's homecoming.",
+  },
+];
+
+export default images
